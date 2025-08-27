0cf00781a7877108d901517d4158cdd513e95e5a
shay <shailstorms@gmail.com>
2025-08-27 15:16:44 -0400

    font stuff

--- app/fonts.tsx
+++ app/fonts.tsx
@@ -2,9 +2,11 @@ import { Oxanium } from "next/font/google";
 import { Quantico } from "next/font/google";
 
 export const oxanium = Oxanium({
-    weight: ["400", "600", "700"]
+    weight: ["400", "600", "700"],
+    subsets: ["latin"]
 })
 
 export const quantico = Quantico({
-    weight: ["400", "700"]
+    weight: ["400", "700"],
+    subsets: ["latin"]
 })
